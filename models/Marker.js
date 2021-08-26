4c965ca774e79e24c42d91786d6bf1ca1033aff8
WibyFabian08 <wibyfabian08@gmail.com>
2021-08-26 11:05:07 -0700

    fix Marker model

--- models/Marker.js
+++ models/Marker.js
@@ -7,6 +7,10 @@ const Marker = new Schema(
       type: String,
       required: true,
     },
+    title: {
+      type: String,
+      required: true,
+    },
     long: {
       type: Number,
       required: true,
