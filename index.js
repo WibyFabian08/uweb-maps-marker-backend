4a27b137f8cad52da5fc6fc18e4fec99bb86c351
WibyFabian08 <wibyfabian08@gmail.com>
2021-08-26 10:55:55 -0700

    fix cors origin

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const markerRouter = require("./routes/marker");
 dotenv.config();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 mongoose.connect("mongodb://localhost:27017/uweb-travel-marker", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
